d2687ffeaf024ab36e652d9e0bf3c4828f22e81e
Christopher Coco <chriscoco1205@gmail.com>
2025-07-10 10:40:34 -0400

    add image updater annotations
    Signed-off-by: Christopher Coco <chriscoco1205@gmail.com>

--- push_test_image.sh
+++ push_test_image.sh
@@ -2,14 +2,16 @@
 # This shell script builds a container image and then pushes it to 
 # the specified repo.
 #
-# Usage: ./push_test_image.sh {TAG} {REPO}
-# TAG argument is required however the REPO argument is optional
+# Usage: ./push_test_image.sh {TAG} {REPO} {CONTAINER_FILE}
+# TAG argument is required however the REPO & CONTAINER_FILE argument is optional
 # If no repo is provided will default to specificed repo on line 31 
+# If no container file is provided will default to just using the current directory
 
 TAG="$1"
 REPO="$2"
-CONTAINER_NAME="webhook_test_dummy"
-IMAGE_NAME="webhook_test_image"
+CONTAINER_FILE="$3"
+CONTAINER_NAME="push_dummy"
+IMAGE_NAME="push_image"
  
 set -u
 
@@ -17,16 +19,20 @@ if test "${TAG}" = ""; then
 	echo "!!  Usage: ./push_test_image.sh {TAG}"
 fi
 
+if test "${CONTAINER_FILE}" = ""; then
+    CONTAINER_FILE="."
+fi
+
 # update container so it is different the blank backup is
 # because macOS used BSD sed not GNU
-sed -E -i '' "s/v[0-9]+\.[0-9]+\.[0-9]+/v${TAG}/g" Containerfile
+# sed -E -i '' "s/v[0-9]+\.[0-9]+\.[0-9]+/v${TAG}/g" ${CONTAINER_FILE}
 
 # remove dummy container so you can create a new one
 # container may not have existed so seeing an error is ok
 sudo podman rm ${CONTAINER_NAME}
 
 # build, run, and kill container
-sudo podman build . -t ${IMAGE_NAME}
+sudo podman build ${CONTAINER_FILE} -t ${IMAGE_NAME}
 sudo podman run -d --name ${CONTAINER_NAME} -p 8000:8000 ${IMAGE_NAME}
 sudo podman kill ${CONTAINER_NAME}
 
